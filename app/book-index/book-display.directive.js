0a296cf51419f991bcc3f2a43ab466a520e57ce4
ditam <ditam@users.noreply.github.com>
2015-12-01 01:28:23 +0100

    add pagination to display directive

--- app/book-index/book-display.directive.js
+++ app/book-index/book-display.directive.js
@@ -6,14 +6,26 @@ angular.module('bookBrowserApp').directive('bookDisplay', function(books){
     restrict: 'E',
     scope: {
         filters: '=',
-        limit: '='
+        limit: '=',
+        paging: '='
     },
     link: function($scope) {
         $scope.results = {
             books: []
         };
         
+        $scope.currentOffset = 0;
+        $scope.previousPage = function(){
+            $scope.currentOffset = Math.max(0,$scope.currentOffset-$scope.limit);
+        };
+        $scope.nextPage = function(){
+            if($scope.currentOffset+$scope.limit<$scope.results.books.length-1){
+                $scope.currentOffset = $scope.currentOffset+$scope.limit;
+            }
+        };
+        
         function updateResults(){
+            $scope.currentOffset = 0;
             var filters = {};
             if($scope.filters){
                 filters.id = $scope.filters.id;
